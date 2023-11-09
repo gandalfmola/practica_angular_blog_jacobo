a7703555ff7bb31413c9c48bf0c1d651395d6dd1
Jacobo Varela <jacobovarelab@gmail.com>
2023-11-09 13:33:16 +0100

    Creado routing y navBar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListaPostsComponent } from './pages/lista-posts/lista-posts.component';
+import { FormularioComponent } from './pages/formulario/formulario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", pathMatch:"full", redirectTo: "posts"},
+  {path:"posts", component:ListaPostsComponent},
+  {path:"blog/new", component:FormularioComponent},
+  {path: "**", redirectTo: "empleados"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaPostsComponent } from './pages/lista-posts/lista-posts.component';
+import { FormularioComponent } from './pages/formulario/formulario.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaPostsComponent,
+    FormularioComponent,
+    NavBarComponent,    
   ],
   imports: [
     BrowserModule,
