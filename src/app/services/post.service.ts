a8a2d34074bdbd23ba4eebac611ca4e162492bb3
Jacobo Varela <jacobovarelab@gmail.com>
2023-11-11 18:38:50 +0100

    Mejorada la muestra de Post iniciales

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -19,36 +19,36 @@ export class PostService {
         categoria:"Pruebas"
       },
       {
-        titulo:"POST DE PRUEBA",
+        titulo:"CAMBIOS DE NORMATIVA EN LA FÓRMULA 1",
         texto:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur beatae quasi quas enim autem. Iure minima recusandae a. Quo animi ratione itaque quos. Voluptatum sint sequi, voluptate beatae quisquam perferendis.",
         autor: "Fulano",
         imagen:"https://previews.123rf.com/images/freud/freud1708/freud170800078/84636787-ilustraci%C3%B3n-de-dibujos-animados-de-un-mono-pensando.jpg",
         fecha: new Date,
-        categoria:"Pruebas"
+        categoria:"Deportes"
       },
       {
-        titulo:"POST DE PRUEBA",
+        titulo:"TOUR DE FRANCIA 2024",
         texto:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur beatae quasi quas enim autem. Iure minima recusandae a. Quo animi ratione itaque quos. Voluptatum sint sequi, voluptate beatae quisquam perferendis.",
         autor: "Fulano",
         imagen:"https://previews.123rf.com/images/freud/freud1708/freud170800078/84636787-ilustraci%C3%B3n-de-dibujos-animados-de-un-mono-pensando.jpg",
         fecha: new Date,
-        categoria:"Pruebas"
+        categoria:"Deportes"
       },
       {
-        titulo:"POST DE PRUEBA",
+        titulo:"¿HABRÁ LEGISLATURA??",
         texto:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur beatae quasi quas enim autem. Iure minima recusandae a. Quo animi ratione itaque quos. Voluptatum sint sequi, voluptate beatae quisquam perferendis.",
         autor: "Fulano",
         imagen:"https://previews.123rf.com/images/freud/freud1708/freud170800078/84636787-ilustraci%C3%B3n-de-dibujos-animados-de-un-mono-pensando.jpg",
         fecha: new Date,
-        categoria:"Pruebas"
+        categoria:"Politica"
       },
       {
-        titulo:"POST DE PRUEBA",
+        titulo:"SUBE EL PRECIO DE LA VIVIENDA",
         texto:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur beatae quasi quas enim autem. Iure minima recusandae a. Quo animi ratione itaque quos. Voluptatum sint sequi, voluptate beatae quisquam perferendis.",
         autor: "Fulano",
         imagen:"https://previews.123rf.com/images/freud/freud1708/freud170800078/84636787-ilustraci%C3%B3n-de-dibujos-animados-de-un-mono-pensando.jpg",
         fecha: new Date,
-        categoria:"Pruebas"
+        categoria:"Economia"
       },
     ]
   }
